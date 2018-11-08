bcc152a90c29be9d7a0d4b1cdc98258dfe397705
llum6003 <44827939+llum6003@users.noreply.github.com>
2018-11-08 01:49:22 -0500

    Add files via upload

---
+++ VariableExample/src/variableExample/variableExample.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package variableExample;
+
+/**
+ * @author leonardolumpford
+ *
+ */
+public class variableExample {
+	// instance variable
+	public String myVar = "instance variable";
+
+	public void myMethod() {
+		// local variable
+		String myVar = "Inside Method";
+		System.out.println(myVar);
+	}
+
+	public static void main(String args[]) {
+		// Creating object
+		variableExample obj = new variableExample();
+
+		/*
+		 * We are calling the method, that changes the value of myVar. We are displaying
+		 * myVar again after the method call, to demonstrate that the local variable
+		 * scope is limited to the method itself.
+		 */
+		System.out.println("Calling Method");
+		obj.myMethod();
+		System.out.println(obj.myVar);
+	}
+}
